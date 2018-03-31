8d5c70b512765063401eb4a19b5b1a226dde31a1
Artem Konyakov <konyakov@gmail.com>
2018-03-31 22:59:46 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,21 +3,30 @@
  * First authored by Brian Cray
  * License: http://creativecommons.org/licenses/by/3.0/
  * Contact the author at http://briancray.com/
+ * Modify by Artem Konyakov
+ * Contact the author at https://konyakov.ru and konyakov@yandex.ru
  */
 
 // db options
-define('DB_NAME', 'your db name');
-define('DB_USER', 'your db usernae');
-define('DB_PASSWORD', 'your db password');
+define('DB_NAME', '***');
+define('DB_USER', '***');
+define('DB_PASSWORD', '***');
 define('DB_HOST', 'localhost');
 define('DB_TABLE', 'shortenedurls');
 
 // connect to database
-mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-mysql_select_db(DB_NAME);
+//$link = mysqli_connect(‘localhost’, $user, $password, $dbname)
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+mysqli_query($link, 'set names utf8');
+
+/* проверка соединения */
+if ($link->connect_errno) {
+    printf("Не удалось подключиться: %s\n", $link->connect_error);
+    exit();
+}
 
 // base location of script (include trailing slash)
-define('BASE_HREF', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('BASE_HREF', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 
 // change to limit short url creation to a single IP
 define('LIMIT_TO_IP', $_SERVER['REMOTE_ADDR']);
@@ -32,7 +41,19 @@ define('CHECK_URL', FALSE);
 define('ALLOWED_CHARS', '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
 // do you want to cache?
-define('CACHE', TRUE);
+define('CACHE', FALSE);
 
 // if so, where will the cache files be stored? (include trailing slash)
 define('CACHE_DIR', dirname(__FILE__) . '/cache/');
+
+function mysqli_result($res,$row=0,$col=0){ 
+    $numrows = mysqli_num_rows($res); 
+    if ($numrows && $row <= ($numrows-1) && $row >=0){
+        mysqli_data_seek($res,$row);
+        $resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
+        if (isset($resrow[$col])){
+            return $resrow[$col];
+        }
+    }
+    return false;
+}
