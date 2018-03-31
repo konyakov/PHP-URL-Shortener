7311c71c0b2ee1e63a96314e14bdf4ad2fc52fc8
Artem Konyakov <konyakov@gmail.com>
2018-03-31 23:05:23 +0300

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -3,6 +3,8 @@
  * First authored by Brian Cray
  * License: http://creativecommons.org/licenses/by/3.0/
  * Contact the author at http://briancray.com/
+ * Modify by Artem Konyakov
+ * Contact the author at https://konyakov.ru and konyakov@yandex.ru
  */
 
 ini_set('display_errors', 0);
@@ -21,7 +23,7 @@ if(CACHE)
 	$long_url = file_get_contents(CACHE_DIR . $shortened_id);
 	if(empty($long_url) || !preg_match('|^https?://|', $long_url))
 	{
-		$long_url = mysql_result(mysql_query('SELECT long_url FROM ' . DB_TABLE . ' WHERE id="' . mysql_real_escape_string($shortened_id) . '"'), 0, 0);
+		$long_url = mysqli_result(mysqli_query($link, 'SELECT long_url FROM ' . DB_TABLE . ' WHERE id="' . mysqli_real_escape_string($link, $shortened_id) . '"'), 0, 0);
 		@mkdir(CACHE_DIR, 0777);
 		$handle = fopen(CACHE_DIR . $shortened_id, 'w+');
 		fwrite($handle, $long_url);
@@ -30,12 +32,12 @@ if(CACHE)
 }
 else
 {
-	$long_url = mysql_result(mysql_query('SELECT long_url FROM ' . DB_TABLE . ' WHERE id="' . mysql_real_escape_string($shortened_id) . '"'), 0, 0);
+	$long_url = mysqli_result(mysqli_query($link, 'SELECT long_url FROM ' . DB_TABLE . ' WHERE id="' . mysqli_real_escape_string($link, $shortened_id) . '"'), 0, 0);
 }
 
 if(TRACK)
 {
-	mysql_query('UPDATE ' . DB_TABLE . ' SET referrals=referrals+1 WHERE id="' . mysql_real_escape_string($shortened_id) . '"');
+	mysqli_query($link, 'UPDATE ' . DB_TABLE . ' SET referrals=referrals+1 WHERE id="' . mysqli_real_escape_string($link, $shortened_id) . '"');
 }
 
 header('HTTP/1.1 301 Moved Permanently');
