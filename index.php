cef611e7ab894a2c890a3c6d0147f8f1c32b4f6e
Artem Konyakov <konyakov@gmail.com>
2018-03-31 23:03:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
 <title>URL shortener</title>
 <meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </html>
 <body>
 <form method="post" action="shorten.php" id="shortener">
 <label for="longurl">URL to shorten</label> <input type="text" name="longurl" id="longurl"> <input type="submit" value="Shorten">
 </form>
 </form>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script>
 $(function () {
 	$('#shortener').submit(function () {
